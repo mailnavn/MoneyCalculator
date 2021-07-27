9e923d9426d2e1e1cbc6924c350260a98f25b381
Naveen K <mailnavn@gmail.com>
2021-07-27 17:58:53 +0100

    Adding the interfaces

---
+++ BusinessLayer/IMoney.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    /// <summary>
+    /// An amount of money in a particular currency.
+    /// </summary>
+    public interface IMoney
+    {
+        /// <summary>
+        /// The amount of money this instance represents.
+        /// </summary>
+        decimal Amount { get; }
+
+        /// <summary>
+        /// The ISO currency code of this instance.
+        /// </summary>
+        string Currency { get; }
+    }
+}
---
+++ BusinessLayer/IMoneyCalculator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    /// <summary>
+    /// Some fun things to do with money.
+    /// </summary>
+    public interface IMoneyCalculator
+    {
+        /// <summary>
+        /// Find the largest amount of money.
+        /// </summary>
+        /// <returns>The <see cref="IMoney"/> instance having the largest amount.</returns>
+        /// <exception cref="ArgumentException">All monies are not in the same currency.</exception>
+        /// <example>{GBP10, GBP20, GBP50} => {GBP50}</example>
+        /// <example>{GBP10, GBP20, EUR50} => exception</example>
+        IMoney Max(IEnumerable<IMoney> monies);
+
+        /// <summary>
+        /// Return one <see cref="IMoney"/> per currency with the sum of all monies of the same currency.
+        /// </summary>
+        /// <example>{GBP10, GBP20, GBP50} => {GBP80}</example>
+        /// <example>{GBP10, GBP20, EUR50} => {GBP30, EUR50}</example>
+        /// <example>{GBP10, USD20, EUR50} => {GBP10, USD20, EUR50}</example>
+        IEnumerable<IMoney> SumPerCurrency(IEnumerable<IMoney> monies);
+
+    }
+}
