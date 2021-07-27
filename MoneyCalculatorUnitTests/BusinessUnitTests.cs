8cf1693855b7da78af2e3f15ae908ddab2276874
Naveen K <mailnavn@gmail.com>
2021-07-27 18:01:50 +0100

    Adding the unit test project

---
+++ MoneyCalculatorUnitTests/BusinessUnitTests.cs
@@ -0,0 +1,28 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MoneyCalculatorUnitTests
+{
+    [TestClass]
+    public class BusinessUnitTests
+    {
+
+
+        [TestInitialize]
+        public void Initialize()
+        {
+
+        }
+
+        [TestCleanup]
+        public void CleanUp()
+        {
+
+        }
+
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
