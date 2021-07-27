fb6ee80d589f7a57468381efc0456446cef7f0f0
Naveen K <mailnavn@gmail.com>
2021-07-27 19:13:57 +0100

    Adding more unit tests for the Max

--- MoneyCalculatorUnitTests/BusinessMoneyCalculatorUnitTests.cs
+++ MoneyCalculatorUnitTests/BusinessMoneyCalculatorUnitTests.cs
@@ -25,6 +25,17 @@ namespace MoneyCalculatorUnitTests
             _MockIMoneyGBP3 = new Mock<IMoney>();
             _MockIMoneyGBP3.Setup(_ => _.Amount).Returns(10.100m);
             _MockIMoneyGBP3.Setup(_ => _.Currency).Returns("GBP");
+            
+            // Negative amount test data
+            _MockIMoneyNegativeAmountGBP1 = new Mock<IMoney>();
+            _MockIMoneyNegativeAmountGBP1.Setup(_ => _.Amount).Returns(-10.100m);
+            _MockIMoneyNegativeAmountGBP1.Setup(_ => _.Currency).Returns("GBP");
+
+            // Negative amount test data
+            _MockIMoneyNegativeAmountGBP2 = new Mock<IMoney>();
+            _MockIMoneyNegativeAmountGBP2.Setup(_ => _.Amount).Returns(-5.100m);
+            _MockIMoneyNegativeAmountGBP2.Setup(_ => _.Currency).Returns("GBP");
+
 
         }
 
@@ -42,9 +53,17 @@ namespace MoneyCalculatorUnitTests
             Assert.ThrowsException<ApplicationException>(() => { moneyCalculator.Max(null); }, "The monies cannot be empty or null");
         }
 
+        [TestMethod]
+        public void TestMaxEmptyListMonies()
+        {
+            MoneyCalculator moneyCalculator = new();
+            Assert.ThrowsException<ApplicationException>(() => { moneyCalculator.Max(new List<IMoney> { }); }, "The monies cannot be empty or null");
+        }
+
+
         /// <summary>
-        /// Test by providing valid monies in GPB currency
-       /// </summary>
+        /// Test by providing valid monies in GPB currency and verify maxium is returned
+        /// </summary>
         [TestMethod]
         public void TestValidMoniesGBP()
         {
@@ -60,11 +79,69 @@ namespace MoneyCalculatorUnitTests
         }
 
 
+
+        /// <summary>
+        /// Test by providing valid monies in GPB with 2 IMoney objects have same amount and currency and verify maxium is returned
+        /// </summary>
+        [TestMethod]
+        public void TestMaxEqualAmount()
+        {
+            // There are 2 equal max amounts added in test data
+            var monies = new List<IMoney> { _MockIMoneyGBP1.Object, _MockIMoneyGBP1.Object, _MockIMoneyGBP3.Object };
+            var result = moneyCalculator.Max(monies);
+
+            // Verify if  100.100 is returned back which is the maximum
+            Assert.AreEqual(100.100m, result.Amount);
+
+            //verify the currency is GBP
+            Assert.AreEqual("GBP", result.Currency);
+
+        }
+
+
+        /// <summary>
+        /// Test by providing monies in GPB containing negative numbers for amount
+        /// </summary>
+        [TestMethod]
+        public void TestMaxNegativeAmount()
+        {
+            // There are 2 equal max amounts added in test data
+            var monies = new List<IMoney> { _MockIMoneyGBP1.Object, _MockIMoneyGBP2.Object, _MockIMoneyNegativeAmountGBP1.Object };
+            var result = moneyCalculator.Max(monies);
+
+            // Verify if  100.100 is returned back which is the maximum
+            Assert.AreEqual(100.100m, result.Amount);
+
+            //verify the currency is GBP
+            Assert.AreEqual("GBP", result.Currency);
+
+        }
+
+
+        /// <summary>
+        /// Test by providing monies in GPB containing only negative numbers for amount
+        /// </summary>
+        [TestMethod]
+        public void TestMaxOnlyNegativeAmounts()
+        {
+            // There are 2 equal max amounts added in test data
+            var monies = new List<IMoney> { _MockIMoneyNegativeAmountGBP1.Object, _MockIMoneyNegativeAmountGBP2.Object };
+            var result = moneyCalculator.Max(monies);
+
+            //verify -5.100m is returned which is maximum 
+            Assert.AreEqual("GBP", result.Currency);
+
+        }
+
+
+
         #region private properties
 
         private Mock<IMoney> _MockIMoneyGBP1;
         private Mock<IMoney> _MockIMoneyGBP2;
         private Mock<IMoney> _MockIMoneyGBP3;
+        private Mock<IMoney> _MockIMoneyNegativeAmountGBP1;
+        private Mock<IMoney> _MockIMoneyNegativeAmountGBP2;
 
         private MoneyCalculator moneyCalculator { get; set; }
         #endregion
